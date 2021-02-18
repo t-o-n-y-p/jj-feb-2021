05b9505935107e48d7f9eaa04fa137c874715dcf
t-o-n-y-p <42129780+t-o-n-y-p@users.noreply.github.com>
2021-02-18 20:16:47 +0300

    Change hello message: add exclamations

--- src/main/java/org/levelp/Test.java
+++ src/main/java/org/levelp/Test.java
@@ -2,7 +2,7 @@ package org.levelp;
 
 public class Test {
     public static void main(String[] arguments) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World!!!");
     }
 
 }
