995e76f98c402c2fd23dcf1d9fe9540376352bbf
t-o-n-y-p <42129780+t-o-n-y-p@users.noreply.github.com>
2021-02-18 20:09:19 +0300

    Rename main args parameter

--- src/main/java/org/levelp/Test.java
+++ src/main/java/org/levelp/Test.java
@@ -1,7 +1,7 @@
 package org.levelp;
 
 public class Test {
-    public static void main(String[] args) {
+    public static void main(String[] parameters) {
         System.out.println("Hello, World!");
     }
 
